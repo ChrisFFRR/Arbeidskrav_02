999bc344a4281a7e46aec477b952803b0bbefdef
Chris FFRR <chris@shotatdawn.net>
2018-04-25 10:21:43 +0200

    Final 1.0.
    Fjernet Parent + primaryStage

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,8 +1,6 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import views.WelcomeView;
 
@@ -25,20 +23,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage)  {
 
-        //Parent root = new Parent() {
-        //};
         WelcomeView welcomeView = new WelcomeView();
-
-
-       // primaryStage.setTitle("Arbeidskrav 02");
-       // primaryStage.setScene(new Scene(root, 600, 600));
 
         Stage quiz = new Stage();
         welcomeView.showWelcome(quiz);
 
         quiz.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
